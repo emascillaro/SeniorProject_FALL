ad1784e78de70777e38691f7452b03390e1f8388
emascillaro <emascillaro@gmail.com>
2020-09-17 15:26:34 -0400

    Fixed error

--- Speech Recognition/Speech_Recognition_Test.py
+++ Speech Recognition/Speech_Recognition_Test.py
@@ -4,12 +4,13 @@ import speech_recognition as sr
 print(sr.__version__)
 
 # Creates a recognizer instance
-r = sr.Recognizer
+r = sr.Recognizer()
 
 # Record Data from File
 harvard = sr.AudioFile('harvard.wav')
 with harvard as source:
-    audio = r.record(source)  # Throwing error that there is a missing positional argument "source"
+    audio = r.record(source)
+
 
 # Check the type of audio in "audio"
 print(type(audio))
