9a06d0a0459376f7c8f1be87d8c4d15b711bcff6
emascillaro <emascillaro@gmail.com>
2020-09-21 19:16:02 -0400

    Background Noise

--- Speech Recognition/Speech_Recognition_Test.py
+++ Speech Recognition/Speech_Recognition_Test.py
@@ -28,10 +28,10 @@ Transcribing speech from an audio file - capturing portions of the file
 # Record Data from File
 with harvard as source:
 
-    # First 4 seconds of Audio recorded in Audio_0_4
+    # First 4 seconds of audio recorded in audio_0_4
     audio_0_4 = r.record(source, duration = 4)
 
-    # Second 4 seconds of Audio recorded in Audio_4_8
+    # Second 4 seconds of audio recorded in audio_4_8
     audio_4_8 = r.record(source, duration = 4)
 
 # Invokes Google Web Speech API & outputs text
@@ -42,7 +42,7 @@ print("Second 4 seconds of audio file to text:", r.recognize_google(audio_4_8))
 
 # Record Data from File
 with harvard as source:
-    # Seconds 3-7 of Audio recorded in Audio_3_7
+    # Seconds 3-7 of audio recorded in audio_3_7
     audio_3_7 = r.record(source, offset = 3, duration = 4)
 
 # Invokes Google Web Speech API & outputs text
@@ -53,8 +53,46 @@ print("Seconds 3-7 of audio file to text (using offset):", r.recognize_google(au
 
 # Record Data from File
 with harvard as source:
-    # Seconds 4.7-7.5 of Audio recorded in Audio_midword
+    # Seconds 4.7-7.5 of audio recorded in audio_midword
     audio_midword = r.record(source, offset = 4.7, duration = 2.8)
 
 # Invokes Google Web Speech API & outputs text
 print("Seconds 4.7-7.5 of audio file to text:", r.recognize_google(audio_midword))
+
+
+"""
+Transcribing speech from an audio file with background noise
+"""
+
+jackhammer = sr.AudioFile('jackhammer.wav')
+
+# Record Data from File
+with jackhammer as source:
+    # Audio w/ background noise recorded in audio_BN
+    audio_BN = r.record(source)
+
+print("Audio file to text (Has Background Noise):", r.recognize_google(audio_BN))
+
+### Adjusting for Background Noise (not perfect)
+
+# Record Data from File
+with jackhammer as source:
+    # Adjusts for background noise
+    r.adjust_for_ambient_noise(source)
+    # Adjusted audio w/ background noise recorded in audio_Adjust_BN
+    audio_Adjust_BN = r.record(source)
+
+print("Audio file to text (Adjusted Background Noise):", r.recognize_google(audio_Adjust_BN))
+
+
+### Adjusting for Background Noise (improved)
+
+# Record Data from File
+with jackhammer as source:
+    # Adjusts for background noise - only reads first 0.5 seconds to calibrate
+    #(won't consume as much audio from the beginning of the file)
+    r.adjust_for_ambient_noise(source, duration = 0.5)
+    # Adjusted audio w/ background noise recorded in audio_Adjust2_BN
+    audio_Adjust2_BN = r.record(source)
+
+print("Audio file to text (Adjusted Background Noise - improved):", r.recognize_google(audio_Adjust2_BN))
