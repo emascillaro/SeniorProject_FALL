4cf3edd5ff8a830e8aa56552e4525a8ac42d1409
emascillaro <emascillaro@gmail.com>
2020-09-24 11:35:50 -0400

    Microphone input to text

--- Speech Recognition/Microphone_Speech_Recognition_Test.py
+++ Speech Recognition/Microphone_Speech_Recognition_Test.py
@@ -9,22 +9,26 @@ Transcribing speech from microphone input
 """
 
 # Lists all the available microphones
-print("Available Microphones:", sr.Microphone.list_microphone_names())
+print("Available Microphones:")
+sr.Microphone.list_microphone_names()
 
 # Gives default Microphone information
-print("DEFAULT MIC INFO")
-pa = pyaudio.PyAudio()
-pa.get_default_input_device_info()
+#print("DEFAULT MIC INFO")
+#pa = pyaudio.PyAudio()
+#pa.get_default_input_device_info()
 
 # Use default system microphone as source,
+print("use default microphone as source:")
 mic = sr.Microphone()
 
 # Use specified microphone as source
 #mic = sr.Microphone(device_index = 2)
 
 # Record Data from Microphone input
+print("record data from microphone input:")
 with mic as source:
     audio = r.listen(source)
 
 # Invokes Google Web Speech API & outputs text
-r.recognize_google(audio)
+print("Invoke Google Web Speech API:")
+print(r.recognize_google(audio))
